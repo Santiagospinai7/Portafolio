c7b2568695362d161a1e634173e0be476ad6249b
Santiagospinai7 <santiagospidrobo15@gmail.com>
2021-10-15 11:04:27 -0500

    Implement styled component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+//Global Style
+import GlobalStyle from '../src/components/GlobalStyle'
 //Import pages
 import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs/>
     </div>
   );
