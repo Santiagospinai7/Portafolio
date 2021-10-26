2f69aad63d411761a71c9ac387810232c6c3d256
Santiagospinai7 <santiagospidrobo15@gmail.com>
2021-10-25 21:12:55 -0500

    Movie Details

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AboutUs from './pages/AboutUs';
 import ContactUs from './pages/ContactUs';
 import OurWork from './pages/OurWork';
 import Nav from './components/Nav'
+import MovieDetail from './pages/MovieDetail';
 //Router
 import { Switch, Route } from 'react-router-dom';
 
@@ -14,21 +15,24 @@ function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <Nav />
-      <Switch>
-        <Route path="/" exact>
-          <AboutUs/>
-        </Route>
-        <Route path="/work">
-          <OurWork />
-        </Route>
-        <Route path="/contact">
-          <ContactUs />
-        </Route>
-      </Switch>
-     
+        <Nav />
+          <Switch>
+            <Route path="/" exact>
+              <AboutUs/>
+            </Route>
+            <Route path="/work" exact>
+              <OurWork />
+            </Route>
+            <Route path="/work/:id">
+              <MovieDetail />
+            </Route>
+            <Route path="/contact">
+              <ContactUs />
+            </Route>
+          </Switch>
     </div>
   );
 }
+
 
 export default App;
