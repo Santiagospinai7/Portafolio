4b9597e002a0bf9279cab12e809e2b5465d39944
Santiagospinai7 <santiagospidrobo15@gmail.com>
2021-10-28 17:59:05 -0500

    Wave animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -4,7 +4,7 @@ import { About, Description, Image, Hide } from '../styles';
 //Framer motion
 import { motion } from 'framer-motion';
 import { titleAnim , fade, photoAnim } from '../animation';
-
+import Wave from './Wave';
 
 const AboutSection = () => {
 
@@ -31,9 +31,12 @@ const AboutSection = () => {
             <Image>
                 <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
             </Image>
+            <Wave />
         </About>
     );
 }
+
+
 
 //Styled component
 
