e188bf3a4d5d4fae135ada2b52e535f98bde5a29
Santiagospinai7 <santiagospidrobo15@gmail.com>
2021-11-02 22:41:37 -0500

    reusable components

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,40 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import { About } from '../styles';
+import Toggle from './Toggle';
 
 const FaqSection = () => {
     return (
         <Faq>
-            <h2>Any Questions <span>FAQ</span></h2>
-            <div className="question">
-                <h4>How Do I Start?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                        Necessitatibus, neque.
-                    </p>
+            <h2>
+                Any Questions <span>FAQ</span>
+            </h2>
+            <Toggle>
+                <div className="question">
+                    <h4>How Do I Start?</h4>
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                                Necessitatibus, neque.
+                            </p>
+                    </div>
+                    <div className="faq-line"></div>
                 </div>
-                <div className="faq-line"></div>
-            </div>
-
+            </Toggle>
+            <Toggle>
             <div className="question">
                 <h4>Daily Schedule</h4>
                 <div className="answer">
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                        Necessitatibus, neque.
-                    </p>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                            Necessitatibus, neque.
+                        </p>
                 </div>
                 <div className="faq-line"></div>
             </div>
-
+            </Toggle>
+            <Toggle>
             <div className="question">
                 <h4>What Products do you offer.</h4>
                 <div className="answer">
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                        Necessitatibus, neque.
-                    </p>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                            Necessitatibus, neque.
+                        </p>
                 </div>
                 <div className="faq-line"></div>
             </div>
+            </Toggle>
         </Faq>
     )
 };
